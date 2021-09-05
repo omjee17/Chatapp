8752b558eeae174faa21f38d7bb2fbe1c0d2be42
omjee17 <singhomjee123@gmail.com>
2021-09-05 19:47:52 +0530

    Basic Message Sending

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 const express=require('express')
+const path=require('path')
+const http=require('http')
+const socketio=require('socket.io')
+
 const app=express()
 const port=3000
+const server=http.createServer(app)
+const io=socketio(server)
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+// set static fodler
+app.use("/",express.static(path.join(__dirname,'public')))
+
+// Run when client connects
+io.on('connection',(socket)=>{
+
+    // Welcome current user
+    socket.emit('message','Welcome to ChatCord!!')
+
+    // Broadcast when a user connects
+    socket.broadcast.emit('message','A user has joined has the chat')
+    // runs when client disconnect
+    socket.on('disconnect',()=>{
+        io.emit('message','A user has left the chat')
+    })
+
+    // listen for chat message
+    socket.on('chatMessage',(msg)=>{
+        io.emit('message',msg)
+    })
+
+})
 
 app.get("/",(req,res)=>{
     res.send("Hello World!!")
 })
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server started at http://localhost:${port}`);   
 })
