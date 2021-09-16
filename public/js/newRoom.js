bdd113656a6bf9f59e57a00d5fbda81d0149e19d
omjee17 <singhomjee123@gmail.com>
2021-09-17 00:19:49 +0530

    NewRomm setup

---
+++ public/js/newRoom.js
@@ -0,0 +1,14 @@
+// let password=prompt("Admin Password:")
+// if(password!==config.password)
+// {
+//     // window.location="/public/newRoom.html"
+// }
+// else{
+//     main()
+// }
+
+
+
+function main(){
+    
+}
